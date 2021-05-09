6df086d9c2dbf4aac5c47cb6fe3478d051f31fe3
mohammed2702 <ameenmohammed2311@gmail.com>
2021-05-09 21:40:55 +0100

    config server updates, to remove nginx config

--- configure_server.sh
+++ configure_server.sh
@@ -4,6 +4,9 @@ sudo chmod -R 775 ~/home/mohammed/umge/backend/media/
 sudo mv umge.conf /etc/apache2/sites-available/
 sudo a2ensite umge.conf
 
+sudo systemctl restart apache2
+sudo systemctl reload apache2
+
 # sudo mv umge_frontend.conf /etc/nginx/sites-enabled/
 
 # sudo systemctl reload apache2 nginx
