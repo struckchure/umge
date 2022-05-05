ff493dfa74352e2c7caa6727e14fe64f2a196006
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-05-05 16:42:08 +0100

    adds migrations

---
+++ backend/migrate.sh
@@ -0,0 +1,7 @@
+python manage.py makemigrations accounts
+python manage.py makemigrations cart
+python manage.py makemigrations delivery
+python manage.py makemigrations locations
+python manage.py makemigrations store
+
+python manage.py migrate
