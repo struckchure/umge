9755e86501034b1184c55cf95546098cd526aba3
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-05-05 16:34:55 +0100

    fix(database): updates database settings

--- frontend/src/components/common/Loader.vue
+++ frontend/src/components/common/Loader.vue
@@ -1,35 +1,32 @@
 <template>
-	<div class="loader flex flex-h-center" :style="{ display: get_display }">
-		<div class="loader-content">
-			<img src="@/assets/logo.png" class="loader-animation">
-			<i class="loader-animation">
-			</i>
+  <div class="loader flex flex-h-center" :style="{ display: get_display }">
+    <div class="loader-content">
+      <img src="@/assets/logo.png" class="loader-animation" />
+      <i class="loader-animation"> </i>
 
-			<p class="form-header mx-auto text-sm md:text-3xl">
-				loading ...
-			</p>
-		</div>
-	</div>
+      <p class="form-header mx-auto text-sm md:text-3xl">loading ...</p>
+    </div>
+  </div>
 </template>
 
 <script>
-	import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 
-	import '@/assets/css/loader.css'
+import "@/assets/css/loader.css";
 
-	export default {
-		name: 'Loader',
-		computed: {
-			...mapGetters({
-				is_loading: 'get_is_loading'
-			}),
-			get_display () {
-				if (this.is_loading === true) {
-					return 'flex'
-				} else {
-					return 'none'
-				}
-			}
-		}
-	}
+export default {
+  name: "Loader",
+  computed: {
+    ...mapGetters({
+      is_loading: "get_is_loading",
+    }),
+    get_display() {
+      if (this.is_loading === true) {
+        return "flex";
+      } else {
+        return "none";
+      }
+    },
+  },
+};
 </script>
